414b889b23e38aafa6f6c1afa80a1b1ce3bbf899
mohammad654 <mohammad.ali.shikhi.55@gmail.com>
2019-02-04 14:20:08 +0100

    t

---
+++ hoofdstukopdrach1.js
@@ -0,0 +1,23 @@
+
+
+document.getElementById("div1").innerText=" Hello World";
+
+console.log("Hello World");
+var x=10;
+if (x>9) {
+    console.log("x Is Groter Dan 9");
+}
+var i=0;
+for (i; i<15; i++) {
+    console.log(" i Heeft Nu De Waarde  "+ i);
+}
+
+var myAuto ={
+    merk:"Ford",
+    type:"Mondeo",
+    aantaWielen:4,
+    kleur:"Rede"
+};
+console.log(myAuto);
+console.log(myAuto.kleur);
+document.getElementById("div2").innerText=myAuto.kleur;
---
+++ proberenjavascript.js
@@ -0,0 +1,6 @@
+
+
+var div1= document.getElementById("div1");
+    div2= document.getElementById("div2");
+    div3= document.getElementById("div3");
+    div4= document.getElementById("div4");
